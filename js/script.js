0a23e938187103e26e146e4f53d56dc8e2491ba3
Trotteriniku <stefanozidda3@gmail.com>
2023-10-09 18:03:06 +0200

    added scount and prezzotot

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-let kilometri =parseInt(prompt("Quanti chilometri intendi percorrere?")) 
-let eta =  parseInt(prompt("Quanti anni hai?")) 
-const prezzoKM = 0.25
+let kilometri =parseInt(prompt("Quanti chilometri intendi percorrere?"));
+let eta =  parseInt(prompt("Quale è la tua età?"));
+const prezzoKM = 0.21;
+let prezzoTot = ( kilometri * prezzoKM);
+let prezzoScon = document.getElementById('testo');
+
+if(isNaN(kilometri) || isNaN(eta)){
+    prezzoScon.innerHTML = 'devi inserire dei numeri';
+    window.location.reload();
+}
+
+if (eta <= 17) {
+    prezzoTot = prezzoTot -(prezzoTot * 20 / 100);
+    prezzoScon.innerHTML=`
+        il costo del tuo biglietto è ${prezzoTot};
+    
+    `  
+    console.log(prezzoTot);
+    
+} else if (eta >= 65){
+    prezzoScon =  prezzoTot -(prezzoTot * 40 / 100);
+    prezzoScon.innerHTML=`
+        il costo del tuo biglietto è ${''} ${prezzoTot}
+    
+    `  
+    console.log(prezzoTot);
+} else{
+    prezzoScon.innerHTML=`
+        il costo del tuo biglietto è ${''} ${prezzoTot}
+    
+    ` 
+}
